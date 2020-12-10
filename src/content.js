ce656d606b23f3882420664986ba3828c1a3a3a2
Matt Jiang <mattljiang@gmail.com>
2020-12-10 14:00:14 -0800

    finish custom patterns functionality

--- src/content.js
+++ src/content.js
@@ -1,35 +1,14 @@
 // get the URL of current tab
 const url = new URL(window.location.href);
 
-// // create a function to check postattendee case
-// const isPostAttendee = () => (url.pathname.startsWith('/postattendee'));
+// Object instead of Array
+// const urlPrefixes = {'zoom.us/j/': true, 'zoom.us/s/': true, 'zoom.us/postattendee': true, 'zoom.us/wc/leave': true, 'prod.liveshare.vsengsaas.visualstudio.com/join': true};                            
+// const matchesUrlPrefix = () => (urlPrefixes[/* prefix that matches our current URL */]) ? true : false;
 
-// // create a function to check web client leave case
-// const isWebClientLeave = () => (url.pathname.startsWith('/wc/leave'));
-
-// // create a function to check success case
-// const isSuccessful = () => (url.searchParams.get('status') === 'success' || 
-//                             (url.hash && url.hash.includes('success')));
-
-const urlPrefixes = ['zoom.us/j/', 'zoom.us/s/', 'zoom.us/postattendee', 'zoom.us/wc/leave', 'prod.liveshare.vsengsaas.visualstudio.com/join'];                            
 // check if our href matches any of the prefixes
+const urlPrefixes = ['zoom.us/j/', 'zoom.us/s/', 'zoom.us/postattendee', 'zoom.us/wc/leave', 'prod.liveshare.vsengsaas.visualstudio.com/join'];                            
 const matchesUrlPrefix = () => urlPrefixes.some((urlPrefix) => url.href.startsWith(`${url.protocol}//${urlPrefix}`));
 
-/*
-            "*://*.zoom.us/j/*",
-            "*://*.zoom.us/s/*",
-            "*://*.zoom.us/postattendee*",
-            "*://*.zoom.us/wc/leave*",
-            "*://prod.liveshare.vsengsaas.visualstudio.com/join*"
-*/
-
-// function checkMatchesAndSendMessage
-// const checkMatchesAndSendMessage = () => {
-//   if (isSuccessful() || isWebClientLeave() || isPostAttendee() || matchesUrlPrefix()) {
-//     // if yes, then send message to background.js
-//     chrome.runtime.sendMessage({ shouldCloseTab: true });
-//   }
-// }
 const checkMatchesAndSendMessage = () => {
   if (matchesUrlPrefix()) {
     // if yes, then send message to background.js
@@ -37,7 +16,7 @@ const checkMatchesAndSendMessage = () => {
   }
 }
 
-// run after 10 seconds, enough time for user to click confirmation box
+// run after a few seconds, enough time for user to click confirmation box
 setTimeout(checkMatchesAndSendMessage, 7000);
 
 
