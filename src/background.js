ee49d2d021dc559b5c116d4da6f92befde8c5a9b
Matt Jiang <mattljiang@gmail.com>
2020-12-10 00:52:14 -0800

    clean up comments

--- src/background.js
+++ src/background.js
@@ -1,7 +1,6 @@
 // Set up the listener for the upcoming message,sender from the content.js
 chrome.runtime.onMessage.addListener((message, sender) => {
   if (message.shouldCloseTab) {
-    // grab the sender's tab id
     // if message is true then close current tab by referencing to sender id
     chrome.tabs.remove(sender.tab.id);
   }
--- src/content.js
+++ src/content.js
@@ -1,6 +1,5 @@
 // get the URL of current tab
 const url = new URL(window.location.href);
-console.log("url: ", url);
 
 // create a function to check postattendee case
 const isPostAttendee = () => (url.pathname.startsWith('/postattendee'));
@@ -15,7 +14,6 @@ const isSuccessful = () => (url.searchParams.get('status') === 'success' ||
 // function checkMatchesAndSendMessage
 const checkMatchesAndSendMessage = () => {
   if (isSuccessful() || isWebClientLeave() || isPostAttendee()) {
-    console.log('met one of our cases');
     // if yes, then send message to background.js
     chrome.runtime.sendMessage({ shouldCloseTab: true });
   }
@@ -23,5 +21,4 @@ const checkMatchesAndSendMessage = () => {
 
 checkMatchesAndSendMessage();
 
-// set timeout (checkMatchesAndSendMessage, 3000ms)
 
