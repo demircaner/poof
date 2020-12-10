6bc9cd52d872bc9241e6ef92640bb409a9a27409
Matt Jiang <mattljiang@gmail.com>
2020-12-10 00:35:56 -0800

    closes Zoom launch page

--- src/content.js
+++ src/content.js
@@ -1,5 +1,27 @@
 // get the URL of current tab
-// check if it matches 
-  // post attendee
-  // wc/leave
-  // #success
+const url = new URL(window.location.href);
+console.log("url: ", url);
+
+// create a function to check postattendee case
+const isPostAttendee = () => (url.pathname.startsWith('/postattendee'));
+
+// create a function to check web client leave case
+const isWebClientLeave = () => (url.pathname.startsWith('/wc/leave'));
+
+// create a function to check success case
+const isSuccessful = () => (url.searchParams.get('status') === 'success' || 
+                            (url.hash && url.hash.includes('success')));
+
+// function checkMatchesAndSendMessage
+const checkMatchesAndSendMessage = () => {
+  if (isSuccessful() || isWebClientLeave() || isPostAttendee()) {
+    console.log('met one of our cases');
+    // if yes, then send message to background.js
+    chrome.runtime.sendMessage({ shouldCloseTab: true });
+  }
+}
+
+checkMatchesAndSendMessage();
+
+// set timeout (checkMatchesAndSendMessage, 3000ms)
+
