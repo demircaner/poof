3769de9e8d9a8178bff362033cc3b2c4f0c5e154
Matt Jiang <mattljiang@gmail.com>
2020-12-10 02:21:13 -0800

    add matchesUrlPrefix functionality

--- src/content.js
+++ src/content.js
@@ -11,15 +11,18 @@ const isWebClientLeave = () => (url.pathname.startsWith('/wc/leave'));
 const isSuccessful = () => (url.searchParams.get('status') === 'success' || 
                             (url.hash && url.hash.includes('success')));
 
+const urlPrefix = 'prod.liveshare.vsengsaas.visualstudio.com/join';                            
+const matchesUrlPrefix = () => (url.href.startsWith(`${url.protocol}//${urlPrefix}`));
+
 // function checkMatchesAndSendMessage
 const checkMatchesAndSendMessage = () => {
-  if (isSuccessful() || isWebClientLeave() || isPostAttendee()) {
+  if (isSuccessful() || isWebClientLeave() || isPostAttendee() || matchesUrlPrefix()) {
     // if yes, then send message to background.js
     chrome.runtime.sendMessage({ shouldCloseTab: true });
   }
 }
 
 // run after 10 seconds, enough time for user to click confirmation box
-setTimeout(checkMatchesAndSendMessage, 10000);
+setTimeout(checkMatchesAndSendMessage, 7000);
 
 
